907969c234c33aa8f34216a42fd65098adcfd9d9
mobarokkkkk <138072473+mobarokkkkk@users.noreply.github.com>
2023-07-09 03:37:39 +0600

    Create mobarok.py

---
+++ mobarok.py
@@ -0,0 +1,7 @@
+http://122.11.163.128:80echo "# Mobarok-king" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mobarokkkkk/Mobarok-king.git
+git push -u origin main
